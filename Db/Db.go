a6155c418ddbb2b265389f14320ad7e2ad522d76
MOHAMMADmiZAN <takbir.jcd@gmail.com>
2022-04-27 01:49:19 +0600

    jwt token and Auth

--- Db/Db.go
+++ Db/Db.go
@@ -2,13 +2,21 @@ package Db
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/kamva/mgm/v3"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	"os"
 )
 
 func Init() {
-	err := mgm.SetDefaultConfig(nil, "go-student-attendance", options.Client().ApplyURI("mongodb://localhost:27017"))
+	err := godotenv.Load(".env")
+	if err != nil {
+		os.Exit(1)
+	}
+	DBNAME := os.Getenv("DATA_BASE_NAME")
+	DBURI := os.Getenv("DATA_BASE_URI")
+	err = mgm.SetDefaultConfig(nil, DBNAME, options.Client().ApplyURI(DBURI))
 	if err != nil {
 		log.Fatal("Database connection error: ", err)
 	} else {
